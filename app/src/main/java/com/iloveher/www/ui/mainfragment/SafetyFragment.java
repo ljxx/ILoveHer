1f2cd5784ce53ae99bd389995dfc9943c3d65fd8
YangLiXiang <yanglixiang@zhaoonline.com>
2016-08-30 16:03:06 +0800

    安全页仿照支付宝九宫格样式布局GridView

--- app/src/main/java/com/iloveher/www/ui/mainfragment/SafetyFragment.java
+++ app/src/main/java/com/iloveher/www/ui/mainfragment/SafetyFragment.java
@@ -1,9 +1,19 @@
 package com.iloveher.www.ui.mainfragment;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
 import com.iloveher.www.R;
 import com.iloveher.www.ui.base.BaseFragment;
 
 public class SafetyFragment extends BaseFragment {
+
+    private GridView mGridView;
+    private String[] mModules;
+    private GridViewAdapter mAdapter;
 
     @Override
     protected int inflateView() {
@@ -12,11 +22,40 @@ public class SafetyFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-
+        mGridView = (GridView) mView.findViewById(R.id.mGridView);
+        mModules = mContext.getResources().getStringArray(R.array.safer_modules);
+        mAdapter = new GridViewAdapter();
+        mGridView.setAdapter(mAdapter);
     }
 
     @Override
     protected void initListen() {
 
+    }
+
+    private class GridViewAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return mModules.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mModules[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            view = View.inflate(mContext,R.layout.safety_gridview_item,null);
+            TextView mtext = (TextView) view.findViewById(R.id.mModuleName);
+            mtext.setText(mModules[position]);
+            return view;
+        }
     }
 }
